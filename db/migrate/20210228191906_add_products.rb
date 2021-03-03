3ccac9704940d2db1f0cc11d426be472f14a9e2e
UwUbulachka <meuw25@gmail.com>
2021-03-03 18:24:32 +0300

    Миграция продукты

---
+++ db/migrate/20210228191906_add_products.rb
@@ -0,0 +1,32 @@
+class AddProducts < ActiveRecord::Migration[5.2]
+  def change
+	Product.create ({
+	  :title => 'Гавайская',
+      :description => 'Это гавайская пицца',
+      :price => 350,
+      :size => 30,
+      :is_spicy => false,
+      :is_veg => false,
+      :is_best_offer => false,
+      :path_to_image => '/images/.jpg'
+      })
+    Product.create ({:title => 'Пепперони',
+      :description => 'Это пицца Пепперони',
+      :price => 450,
+      :size => 30,
+      :is_spicy => false,
+      :is_veg => false,
+      :is_best_offer => true,
+      :path_to_image => '/images/.jpg'
+      })
+    Product.create ({:title => 'Вегетарианская',
+      :description => 'Это вегетарианская пицца',
+      :price => 400,
+      :size => 30,
+      :is_spicy => false,
+      :is_veg => true,
+      :is_best_offer => false,
+      :path_to_image => '/images/.jpg'
+  })
+  end
+end
